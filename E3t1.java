5a6f3a366034f6a18bd7f8a4f920a918b04028fc
Rubén Perblac <yhana.kromx@gmail.com>
2022-11-05 22:59:56 +0100

    Add files via upload

---
+++ E3t1.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author rperblac
+ */
+public class E3t1 {
+    public static void main(String[] args) {
+        int numeroPaginas;
+        boolean esRepetidor;
+        int perimetro;// si usara decimales sería float
+        char letraPuerta;
+        long cuadradodeSuma500primos;
+    }
+    
+}
