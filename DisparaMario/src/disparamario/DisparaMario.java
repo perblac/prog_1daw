56ae686208ebe15dc22e45c4bca14c528dfa0b75
Rubén Perblac <yhana.kromx@gmail.com>
2022-11-07 21:36:18 +0100

    Add files via upload

--- DisparaMario/src/disparamario/DisparaMario.java
+++ DisparaMario/src/disparamario/DisparaMario.java
@@ -11,11 +11,10 @@ import bpc.daw.mario.*;
 public class DisparaMario {
 
     public static void main(String[] args) {
-        Toolkit t = Toolkit.getDefaultToolkit();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double h= screenSize.getHeight();
         double w= screenSize.getWidth();
-        Mario m = new Mario(0,0);
+        Mario m = new Mario(100,0);
         Cañon c = new Cañon((int)w-40,(int)h/2);
         Disparo p = new Disparo((int)w-40,(int)h/2,-100,(int) h/2);
         boolean haciaAbajo= true;
@@ -25,7 +24,7 @@ public class DisparaMario {
             if (haciaAbajo){
                 if (m.getY()<40)
                 {
-                    m.correrHacia(0, (int)h);
+                    m.correrHacia(100, (int)h);
                 }
                 if (m.getY()>(int)(h-40)){
                 
@@ -35,7 +34,7 @@ public class DisparaMario {
             {
                 if (m.getY()>(int)(h-40))
                 {
-                    m.correrHacia(0, 0);
+                    m.correrHacia(100, 0);
                 }
                 
                 if (m.getY()<40){
@@ -48,7 +47,7 @@ public class DisparaMario {
                 m.eliminar();
                 ++muertes;
                 System.out.print(muertes+" ");
-                m = new Mario(0,0);                
+                m = new Mario(100,0);                
             }
             if (p.getX()<10)
             {
