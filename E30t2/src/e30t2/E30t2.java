81a0a3385d3c032c29ba1a907cfc7844e637e99c
Rubén Perblac <yhana.kromx@gmail.com>
2022-11-18 17:16:54 +0100

    Add files via upload

---
+++ E30t2/src/e30t2/E30t2.java
@@ -0,0 +1,17 @@
+package e30t2;
+
+/**Enunciado: Busca en Internet cuándo se lanzan las siguientes excepciones, e indica si son de tipo RuntimeException o CheckedException.
+    a) SQLException
+    b) IllegalArgumentException
+    c) IllegalStateException
+ *
+ * @author rperblac
+ */
+public class E30t2 {
+    public static void main(String[] args) {
+        System.out.println("    a) SQLException:\t\t\tCheckedException\n" +
+"    b) IllegalArgumentException:\tRuntimeException\n" +
+"    c) IllegalStateException:\t\tRuntimeException");
+    }
+    
+}
