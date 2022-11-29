88a1f9ccfaf874098e129276068262391190592c
Rubén Perblac <yhana.kromx@gmail.com>
2022-11-29 18:02:04 +0100

    Update E1t3.java

--- E1t3/src/e1t3/E1t3.java
+++ E1t3/src/e1t3/E1t3.java
@@ -1,9 +1,6 @@
 package e1t3;
 
-/** Enunciado: Realiza un programa en el que haya 5 archivos mp3 y se muestre en pantalla una
-lista numerada con los títulos de las 5 canciones (por ejemplo: 1 – Jackson.mp3). El usuario
-introducirá un número del 1 al 5 y se usará el bloque switch-case para reproducir la canción
-elegida por el usuario.
+/** Enunciado: Realiza un programa en el que haya 5 archivos mp3 y se muestre en pantalla una lista numerada con los títulos de las 5 canciones (por ejemplo: 1 – Jackson.mp3). El usuario introducirá un número del 1 al 5 y se usará el bloque switch-case para reproducir la canción elegida por el usuario.
  *
  * @author rperblac
  */
