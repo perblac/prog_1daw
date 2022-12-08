871d3a3c38af9288aa5672f488b05a77cd2d8280
Rubén Perblac <yhana.kromx@gmail.com>
2022-12-08 16:50:25 +0100

    Add files via upload

--- E6t3/src/e6t3/E6t3.java
+++ E6t3/src/e6t3/E6t3.java
@@ -7,10 +7,54 @@ otra tecla, finalizará.
  *
  * @author rperblac
  */
+import bpc.daw.consola.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class E6t3 {
 
     public static void main(String[] args) {
-        
+        String entrada;
+        Consola cons = new Consola();
+        Teclado tecl = cons.getTeclado();
+        CapaTexto capat = cons.getCapaTexto();
+        CapaFondo capaf = cons.getCapaFondo();
+        BufferedImage imagen = null;
+        File cargarimagen;
+        char leetecla;
+        boolean imagenvalida = false; // ¿Es la ruta a la imagen correcta?
+        boolean salir; // ¿Salimos del programa?
+        do  // Bucle principal
+        {            
+            do  // Bucle de entrada de imagen
+            {
+                entrada = null;
+                try
+                {
+                    capat.println("Introduzca la ruta a una imagen: ");
+                    entrada = tecl.leerCadenaCaracteres();
+                    cargarimagen = new File(entrada);
+                    imagen = ImageIO.read(cargarimagen);
+                    if (cargarimagen.exists()) imagenvalida = true;
+                } catch (IOException e)
+                {
+                    capat.println(e.getLocalizedMessage());
+                    imagenvalida = false;
+                }
+            } while (!imagenvalida);
+            
+            // Ponemos la imagen como fondo
+            FondoImagen fondo = new FondoImagen(imagen);
+            fondo.setEscalado(true);            
+            capaf.setFondo(fondo);
+                        
+            capat.println("¿Desea poner otra imagen? (S/N)");            
+            leetecla = tecl.leerCaracter();
+            salir = ((leetecla != 'S') && (leetecla != 's')); // Solo salimos si la tecla pulsada es 'S' o 's'
+            
+        } while (!salir);
     }
     
 }
