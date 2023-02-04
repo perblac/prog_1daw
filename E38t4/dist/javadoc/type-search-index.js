de3cda1b5405da785447b99e1d19a8383ffd5bf8
perblac <yhana.kromx@gmail.com>
2023-02-04 22:01:58 +0100

    Add files via upload

---
+++ E38t4/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"gente","l":"e38t4"},{"p":"gente","l":"Persona"}];updateSearchResults();
---
+++ E38t4/src/gente/Persona.java
@@ -0,0 +1,54 @@
+
+package gente;
+
+/**
+ * Persona comparable con otras por su altura
+ * @author rperblac
+ */
+public class Persona implements Comparable<Persona> {
+    private String nombre;
+    private double altura;
+    /**
+     * Crea una persona con un nombre y una altura aleatoria entre 1.50 y 2.10 metros.
+     * @param nombre nombre para la persona
+     */
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        this.altura = Math.round((((Math.random()*60)+150)/100)*100d)/100d;
+    }
+    /**
+     * Devuelve el nombre de la persona
+     * @return nombre de la persona
+     */
+    public String getNombre() {
+        return this.nombre;
+    }
+    /**
+     * Devuelve la altura de la persona
+     * @return altura de la persona
+     */
+    public double getAltura() {        
+        return this.altura;
+    }
+    
+    /**
+     * Compares this object with the specified object for order. Returns a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     * @param o persona a comparar
+     * @return a negative integer, zero, or a positive integer as this object is less than, equal to, or greater than the specified object.
+     */
+    @Override/*
+    public int compareTo(Persona o) {
+        int altA = (int) (this.altura*100);
+        int altB = (int) (o.getAltura()*100);
+        int diferencia = 0;
+        if (altA<altB) diferencia = -(altB-altA);
+        if (altA>altB) diferencia = altA-altB;
+        return diferencia;
+    }*/
+    public int compareTo(Persona o) {
+        String nomA = this.getNombre();
+        String nomB = o.getNombre();        
+        return nomA.compareTo(nomB);
+    }
+
+}
