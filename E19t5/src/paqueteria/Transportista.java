f859c332910276f8ba2f8e6290947de67df3730a
perblac <yhana.kromx@gmail.com>
2023-03-07 21:39:11 +0100

    Add files via upload

---
+++ E19t5/src/paqueteria/Transportista.java
@@ -0,0 +1,61 @@
+
+package paqueteria;
+
+import java.util.List;
+
+/**
+ * Representa una persona que puede enviar un paquete a su destino. Posee una
+ * lista con todos los paquetes que tiene que enviar y además tiene una
+ * propiedad que indica el número de minutos que tarda en enviar un paquete.
+ *
+ * @author rperblac
+ */
+public class Transportista {
+    /**
+     * Lista de paquetes a enviar
+     */
+    private List<Paquete> paquetes;
+    /**
+     * Minutos que tarda en enviar un paquete
+     */
+    private long tiempoEntrega;
+    /**
+     * Crea un transportista que tarda en enviar un paquete el número de minutos
+     * que se pasa como parámetro, y no tiene ningún paquete asignado.
+     *
+     * @param te tiempo que tarda en enviar un paquete
+     */
+    public Transportista(long te) {
+        this.tiempoEntrega = te;
+        this.paquetes = new java.util.ArrayList<>();
+    }
+    /**
+     * Recibe un paquete y lo guarda en su camión.
+     * @param p paquete recibido
+     */
+    public void subirCamion(Paquete p) {
+        this.paquetes.add(p);
+    }
+    /**
+     * Simula el envío de paquetes por el transportista.
+     */
+    public void enviar() {
+        for (Paquete p:this.paquetes)
+        {
+            try {
+                Thread.sleep(this.tiempoEntrega);
+            } catch (InterruptedException ex) {
+                System.out.println("Error en Thread.sleep(long): " + ex.getMessage());
+            }
+            System.out.println("El paquete " + p.getProducto() + " con prioridad "
+                    + p.getPrioridad() + " ha llegado a: " + p.getDireccionDestino());
+        }
+    }
+    /**
+     * Devuelve la lista de paquetes en el camión del transportista
+     * @return lista de paquetes
+     */
+    public List<Paquete> getPaquetes() {
+        return this.paquetes;
+    }
+}
---
+++ E19t5/test/paqueteria/TransportistaTest.java
@@ -0,0 +1,81 @@
+
+package paqueteria;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests de la clase Transportista
+ * @author rperblac
+ */
+public class TransportistaTest {
+    long tiempo = 500;
+    Transportista t = new Transportista(tiempo);
+    String producto = "cosa";
+    String destino = "una casa";
+    int prioridad = Paquete.ALTA;
+    Paquete p = new Paquete(producto, destino, prioridad);
+
+    public TransportistaTest() {
+    }
+    
+    @Test
+    public void testTransportista_long() {
+        assertEquals("paqueteria.Transportista", t.getClass().getName());
+    }
+
+    @Test
+    public void testSubirCamion() {
+        t.subirCamion(p);
+        ArrayList<Paquete> lista = (ArrayList) t.getPaquetes();
+        assertTrue(lista.contains(p));
+    }
+
+    @Test
+    public void testEnviar() {
+        t.subirCamion(p);
+        File test = null;
+        try {
+            test = File.createTempFile("test", ".txt");
+        } catch (IOException ex) {
+            System.out.println("Error, no se pudo crear el archivo de test" + ex.getMessage());
+            fail("Error en el test (creación de archivo test.txt temporal");
+        }
+        PrintStream salida = null;
+        try {
+            salida = new PrintStream(test);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error, no se pudo crear el archivo de test" + ex.getMessage());
+            fail("Error en el test (creación de archivo test.txt para PrintStream)");
+        }
+        System.setOut(salida);
+        t.enviar();
+        String cadena = "";
+        try {
+            cadena = new Scanner(test).nextLine();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error, no se pudo crear el archivo de test" + ex.getMessage());
+            fail("Error en el test (creación de archivo test.txt para Scanner)");
+        }
+        String esperada = "El paquete " + p.getProducto() + " con prioridad "
+                    + p.getPrioridad() + " ha llegado a: " + p.getDireccionDestino();
+        assertEquals(esperada, cadena);        
+        salida.close();
+        test.delete();
+    }
+
+    @Test
+    public void testGetPaquetes() {
+        t.subirCamion(p);
+        assertEquals(1, t.getPaquetes().size());
+    }
+
+}
