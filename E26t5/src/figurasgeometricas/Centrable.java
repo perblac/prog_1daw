674684337f19ecbe887cd658365ad9a73e046399
perblac <yhana.kromx@gmail.com>
2023-03-10 16:27:25 +0100

    Add files via upload

---
+++ E26t5/src/figurasgeometricas/Centrable.java
@@ -0,0 +1,17 @@
+
+package figurasgeometricas;
+
+import java.awt.Point;
+
+/**
+ * Un objeto que tiene un centro.
+ * @author rperblac
+ */
+
+public interface Centrable {
+    /**
+     * Devuelve el centro de la figura
+     * @return Point del centro
+     */
+    public Point getCentro();
+}
---
+++ E26t5/src/figurasgeometricas/FiguraGeometrica.java
@@ -0,0 +1,40 @@
+
+package figurasgeometricas;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ * Una figura geométrica cualquiera.
+ * @author rperblac
+ */
+
+public interface FiguraGeometrica extends Coloreable{
+    /**
+     * Devuelve el área de la figura
+     * @return área de la figura
+     */
+    public double getArea();
+    /**
+     * Devuelve el perímetro de la figura
+     * @return perímetro de la figura
+     */
+    public int getPerimetro();
+    /**
+     * Recibe un objeto Graphics y dibuja con él la figura
+     * @param g objeto Graphics donde se dibujará la figura
+     */
+    public void dibujar(Graphics g);
+    /**
+     * Devuelve un cuadrado que tenga igual área que la figura. Dicho cuadrado
+     * tiene como esquina superior izquierda el Point pasado como parámetro.
+     *
+     * @param esquinaSuperiorIzquierda esquina superior izquierda del cuadrado que se devolverá
+     * @return cuadrado de igual área que la figura
+     */
+    public default Cuadrado crearCuadradoIgualArea(Point esquinaSuperiorIzquierda) {        
+        double lado = Math.sqrt(this.getArea());
+        return new Cuadrado(esquinasuperiorIzquierda, Math.round(lado), Color.black);
+    }
+}
