92041d1eba38cc21e6e1a239a1b817c7cbd67172
perblac <yhana.kromx@gmail.com>
2023-03-10 17:28:01 +0100

    Update TransporteAnimales.java

--- E25t5/src/daw/zoo/TransporteAnimales.java
+++ E25t5/src/daw/zoo/TransporteAnimales.java
@@ -12,42 +12,37 @@ public class TransporteAnimales extends Jaula{
      * Máximo de animales admitido
      */
     static int maxAnimales = 6;
+    
     /**
      * Peso máximo admitido
      */
     static int maxPeso = 500;
+    
     /**
      * Crea un transporte de animales, para 6 animales o 500 kg.
      */
     public TransporteAnimales() {       
         super(maxAnimales, maxPeso);        
     }
+    
     /**
-     * Añade un animal a la jaula, solo si hay una plaza disponible para él y no se supera el peso máximo
-     * @param a animal a añadir
-     * @return true si se pudo añadir al animal
+     * Devuelve si el animal recibido como parámetro puede ser añadido o no, a la jaula
+     * @param a animal que se quiere añadir
+     * @return true si el animal se puede añadir a la jaula
      */
-    @Override
-    public boolean añadir(Animal a) {
+    public boolean comprobarDisponibilidadPlaza(Animal a) {
         boolean r = false;
         if (!this.animales.isEmpty())
         {
             Class c = this.animales.iterator().next().getClass();
             if (c.isInstance(a))
             {
-                r = (this.comprobarDisponibilidadPlaza())
-                && ((this.getPesoActual() + a.getPeso()) <= this.getPesoMaximo());
+                r = (this.getNumeroAnimales() < this.getCapacidadMaxima()) && ((this.getPesoActual() + a.getPeso()) <= this.getPesoMaximo());
             }
         }
         else
         {
-            r = (this.comprobarDisponibilidadPlaza())
-                && ((this.getPesoActual() + a.getPeso()) <= this.getPesoMaximo());
-        }        
-        if (r)
-        {
-            a.setEncerrado(true);
-            this.animales.add(a);
+            r = (this.getNumeroAnimales() < this.getCapacidadMaxima()) && ((this.getPesoActual() + a.getPeso()) <= this.getPesoMaximo());
         }
         return r;
     }
